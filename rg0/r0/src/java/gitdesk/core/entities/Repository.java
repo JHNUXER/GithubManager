bc7b7fc32a45f3e07ace59c862e69d868db154b2
John Kayne <johnhoganuke2@gmail.com>
2017-03-08 14:54:40 -0500

    Update Repository.java

--- rg0/r0/src/java/gitdesk/core/entities/Repository.java
+++ rg0/r0/src/java/gitdesk/core/entities/Repository.java
@@ -3,11 +3,25 @@ package gitdesk.core.entities;
 public class Repository {
   protected String name;
   protected URL url;
+  protected User author;
+
+  public Repository(URL url,User author) {
+    String s = url.toString();
+    int k = ;
+    this.name = "";
+    for (int i = s.lastInstanceOf("/",s.length()-1); i < s.length()-1; i++) {
+      this.name += s.charAt(i);
+    }
+    this.author = author;
+  }
   
   public String getName() {
     return name;
   }
-  public String getURL() {
+  public URL getURL() {
     return url;
+  }
+  public User getAuthor() {
+    return author;
   }
 }
