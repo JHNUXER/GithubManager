a4c8ea4b0be249f50828550f1c1ff6f7ad6e876c
John Kayne <johnhoganuke2@gmail.com>
2017-03-08 14:42:23 -0500

    Create Repository.java

---
+++ rg0/r0/src/java/gitdesk/core/entities/Repository.java
@@ -0,0 +1,13 @@
+package gitdesk.core.entities;
+
+public class Repository {
+  protected String name;
+  protected URL url;
+  
+  public String getName() {
+    return name;
+  }
+  public String getURL() {
+    return url;
+  }
+}
